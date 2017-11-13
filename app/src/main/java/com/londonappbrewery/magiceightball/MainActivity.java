380a0acab9585d1df3879b4b40c929c1ee8e46ab
avl3196 <avl@beol.net>
2017-11-13 14:38:26 -0500

    commit

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,13 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.lang.reflect.Array;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button askButton = findViewById(R.id.ask_button);
+        final ImageView ballDisplay = findViewById(R.id.image_eightball);
+        final int[] ballArray = {R.drawable.ball1,
+                           R.drawable.ball2,
+                           R.drawable.ball3,
+                           R.drawable.ball4,
+                           R.drawable.ball5};
+
+        askButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("MagicEightBall", "the button has been pressed!");
+
+                Random randomNumberGenerator = new Random();
+                int number = randomNumberGenerator.nextInt(5);
+                Log.d("MagicEightBall", "the left random number is: " + number);
+
+                ballDisplay.setImageResource(ballArray[number]);
+            }
+        });
     }
 }
